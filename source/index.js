ad1da4c48b1df4da8b2a4f21377eec3f9f99d2e2
Kyle Mitchell <kyle@kemitchell.com>
2015-04-14 00:45:47 -0700

    simplify

--- source/index.js
+++ source/index.js
@@ -1,56 +1,49 @@
-var Immutable = require('immutable');
+var clone = require('clone');
 
 var flatten = function(form, list, depth) {
-  return form.get('content').reduce(function(list, element) {
+  return form.content.reduce(function(list, element) {
     var newContainer;
-    if (typeof element.has === 'function' && element.has('inclusion')) {
+    if (element.hasOwnProperty('form')) {
       newContainer = {
         depth: depth + 1,
         content: [],
-        numbering: element.get('numbering')
+        numbering: element.numbering
       };
-      if (element.has('heading')) {
-        newContainer.heading = element.get('heading');
+      if (element.hasOwnProperty('heading')) {
+        newContainer.heading = element.heading;
       }
-      var conspicuousKeys = ['inclusion', 'conspicuous'];
-      if (element.hasIn(conspicuousKeys)) {
-        newContainer.conspicuous = element.getIn(conspicuousKeys);
+      if (element.form.hasOwnProperty('conspicuous')) {
+        newContainer.conspicuous = element.form.conspicuous;
       }
-      return flatten(
-        element.get('inclusion'),
-        list.push(Immutable.fromJS(newContainer)),
-        depth + 1
-      );
+      list.push(newContainer);
+      return flatten(element.form, list, depth + 1);
     } else {
-      var last = list.last();
+      var listLength = list.length;
+      var last = list[listLength - 1];
       if (
         !last ||
-        !last.has('depth') ||
-        last.get('depth') !== depth
+        !last.hasOwnProperty('depth') ||
+        last.depth !== depth
       ) {
         newContainer = {
           depth: depth,
           content: []
         };
-        if (form.has('conspicuous')) {
-          newContainer.conspicuous = form.get('conspicuous');
+        if (form.hasOwnProperty('conspicuous')) {
+          newContainer.conspicuous = form.conspicuous;
         }
         newContainer.content.push(element);
-        return list.push(Immutable.fromJS(newContainer));
+        list.push(newContainer);
       } else {
-        var lastIndex = list.count() - 1;
-        return list.updateIn([lastIndex, 'content'], function(last) {
-          return last.push(element);
-        });
+        list[listLength - 1].content.push(element);
       }
+      return list;
     }
   }, list);
 };
 
-var emptyList = Immutable.List();
-
 module.exports = function(form) {
-  return flatten(form, emptyList, 1);
+  return flatten(clone(form), [], 1);
 };
 
-module.exports.version = '0.3.0';
+module.exports.version = '1.0.0-rc1';
--- test/flatten.test.js
+++ test/flatten.test.js
@@ -1,5 +1,4 @@
 /* jshint mocha: true */
-var Immutable = require('immutable');
 var expect = require('chai').expect;
 var resolve = require('commonform-resolve');
 var number = require('commonform-number');
@@ -13,63 +12,56 @@ var num = function(sn, sof, en, eof) {
   };
 };
 
-var noValues = Immutable.Map();
-
 describe('flatten', function() {
   describe('integration test', function() {
     before(function() {
-      var form = Immutable.fromJS({
+      var form = {
         content: [
           'before',
           {
             heading: 'A',
-            inclusion: {
-              conspicuous: 'true',
+            form: {
+              conspicuous: 'yes',
               content: [
                 'before',
-                {inclusion: {content: ['B']}},
-                {inclusion: {content: ['C']}},
+                {form: {content: ['B']}},
+                {form: {content: ['C']}},
                 'between',
-                {inclusion: {content: ['D']}},
-                {inclusion: {content: ['E']}},
-                'after'
-              ]
-            }
-          },
-          'after'
-        ]
-      });
-      var numbering = number(form);
-      var resolved = resolve(form, noValues, numbering);
+                {form: {content: ['D']}},
+                {form: {content: ['E']}},
+                'after']}},
+          'after']};
+      var numberings = number(form);
+      var resolved = resolve(form, {}, numberings);
       this.flattened = flatten(resolved);
     });
 
     var results = [
       {depth: 1, content: ['before']},
         {depth: 2, heading: 'A', content: ['before'],
-            numbering: [num(1, 1, 1, 1)], conspicuous: 'true'},
+            numbering: [num(1, 1, 1, 1)], conspicuous: 'yes'},
           {depth: 3, content: ['B'], numbering: [
             num(1, 1, 1, 1), num(1, 2, 1, 2)]},
           {depth: 3, content: ['C'], numbering: [
             num(1, 1, 1, 1), num(1, 2, 2, 2)]},
-        {depth: 2, content: ['between'], conspicuous: 'true'},
+        {depth: 2, content: ['between'], conspicuous: 'yes'},
           {depth: 3, content: ['D'], numbering: [
             num(1, 1, 1, 1), num(2, 2, 1, 2)]},
           {depth: 3, content: ['E'], numbering: [
             num(1, 1, 1, 1), num(2, 2, 2, 2)]},
-        {depth: 2, content: ['after'], conspicuous: 'true'},
+        {depth: 2, content: ['after'], conspicuous: 'yes'},
       {depth: 1, content: ['after']},
     ];
 
     results.forEach(function(object, index) {
       it('element ' + index + ' is as expected', function() {
-        expect(this.flattened.get(index).toJS())
+        expect(this.flattened[index])
           .to.eql(object);
       });
     });
 
     it('results length is ' + results.length, function() {
-      expect(this.flattened.count())
+      expect(this.flattened.length)
         .to.equal(results.length);
     });
   });
