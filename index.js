4d1fd76aa184a4dccfacfc04c2e160eaaff9a828
Kyle E. Mitchell <kyle@kemitchell.com>
2019-05-31 13:08:34 -0700

    Refactor

--- index.js
+++ index.js
@@ -44,8 +44,6 @@ var flatten = function (form, list, depth) {
 }
 
 module.exports = function (form, values) {
-  if (!values) {
-    values = []
-  }
+  if (!values) values = []
   return flatten(resolve(form, values), [], 1)
 }
